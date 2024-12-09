cdfe7a14d3107f22299b4a4756c0fc4d3b48f6b2
mayaraa2007 <00001107865967sp@al.educacao.sp.gov.br>
2024-12-09 15:59:44 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Pop',
+    'Qual é a indústria da musica?',
+    'A Taylor Swift'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o primeiro grupo global pop do mundo?',
+    'O Now United'
+)
+
+criaCartao(
+    'Série',
+    'Quantas temporadas tem Greys Anatomy Atualmente?',
+    '21'
+)
+
+criaCartao(
+    'Taylor Swift',
+    'O que foi a "The Eras Tour"?',
+    'The Eras Tour foi a maior turne da historia'
+)
